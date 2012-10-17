77ed32b95c60d207d43cc18596ffba28f71bf68e
Matt Priour <mpriour@opengeo.org>
2012-10-17 09:54:06 -0500

    set default tranisitions to none

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-﻿$.mobile.page.prototype.options.headerTheme = "a";
+﻿$(document).ready(function(){
+    $.mobile.defaultDialogTransition = 'none';
+    $.mobile.defaultPageTransition = 'none';
+});
+$.mobile.page.prototype.options.headerTheme = "a";
 $.mobile.page.prototype.options.contentTheme = "a";
 $.mobile.page.prototype.options.footerTheme = "a";
 $("div[data-role='page']").live('pagecreate', function (event) {
