c5736ee6275c5b3a02b1b0f023a6da577a1c54c5
Matt Priour <mpriour@opengeo.org>
2012-10-17 09:50:32 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+﻿$.mobile.page.prototype.options.headerTheme = "a";
+$.mobile.page.prototype.options.contentTheme = "a";
+$.mobile.page.prototype.options.footerTheme = "a";
+$("div[data-role='page']").live('pagecreate', function (event) {
+    $("a[data-rel='back']").remove();
+});
